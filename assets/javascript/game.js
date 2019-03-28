c6167c89421027e1fb75ce6483d6c44197e74baa
AlphaLy <lyalpha2401@gmail.com>
2019-03-28 11:39:26 -0500

    4th Assignment Crystal-Collection-Game using jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,124 @@
+// Environment variables
+
+
+
+var crystal = {
+
+	crystalOne: {
+
+		name: "crystal1",
+		value: 0
+	},
+
+	crystaltwo: {
+
+		name: "crystal2",
+		value: 0
+	},
+
+	crystalthree: {
+
+		name: "crystal3",
+		value: 0
+	},
+
+	crystalfour: {
+
+		name: "crystal4",
+		value: 0
+	},
+
+}
+
+
+var wincount = 0;
+var lostcount = 0;
+var goal = 0;
+var score = 0;
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------
+
+
+
+
+// Functions
+
+var randomNumber = function(min,max){
+
+	return math.Floor(Math.random()*(max-min+1)) + min;
+}
+
+
+var gameredy function(){
+
+	var score = 0;
+	var goal  = randomNumber(19,20);
+
+	crystal.crystal1.value = randomNumber(1,12);
+	crystal.crystal2.value = randomNumber(1,12);
+	crystal.crystal3.value = randomNumber(1,12);
+	crystal.crystal4.value = randomNumber(1,12);
+
+
+
+
+}
+
+
+
+// jQuery click Functions
+
+ $("#one").click(function(){
+
+ 	alert("alpha");
+
+ });
+
+
+ $("#two").click(function(){
+ 	alert("beta");
+
+ });
+
+
+ $("#three").click(function(){
+	alert("gamma");
+ });
+
+ $("#four").click(function(){
+	alert("delta");
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
