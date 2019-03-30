9bdf968a397da6d384074b690b12680fbdb8280b
AlphaLy <lyalpha2401@gmail.com>
2019-03-30 01:25:30 -0500

    4th Assignment Crystal-Collection-Game using jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+
+
+
 // Environment variables
 
 
@@ -32,18 +35,9 @@ var crystal = {
 
 
 var wincount = 0;
-var lostcount = 0;
-var goal = 0;
-var score = 0;
-
-
-
-
-
-
-
-
-
+var losscount = 0;
+var goaltoreach = 0;
+var mycurrentscore = 0;
 
 // -------------------------------------------------------
 
@@ -52,54 +46,120 @@ var score = 0;
 
 // Functions
 
-var randomNumber = function(min,max){
+// var randomNumber = function(min,max){
 
-	return math.Floor(Math.random()*(max-min+1)) + min;
+// 	return Math.floor(Math.random()*(max-min+1)) + min;
+// }
+
+
+var gameready = function(){
+
+	// the function below was outside
+
+	var randomNumber = function(min,max){
+
+	return Math.floor(Math.random()*(max-min+1)) + min;
+}
+
+	var goaltoreach  = randomNumber(19,120);
+
+	crystal.crystalOne.value = randomNumber(1,12);
+	crystal.crystaltwo.value = randomNumber(1,12);
+	crystal.crystalthree.value = randomNumber(1,12);
+	crystal.crystalfour.value = randomNumber(1,12);
+
+	$("#currentscore").html(mycurrentscore);
+	$("#goaltoreach").html(goaltoreach);
+	$("#losscount").html(losscount);
+	$("#wincount").html(wincount);
+
 }
 
 
-var gameredy function(){
+gameready();
 
-	var score = 0;
-	var goal  = randomNumber(19,20);
-
-	crystal.crystal1.value = randomNumber(1,12);
-	crystal.crystal2.value = randomNumber(1,12);
-	crystal.crystal3.value = randomNumber(1,12);
-	crystal.crystal4.value = randomNumber(1,12);
-
-
-
-
-}
-
-
+var onevalue = crystal.crystalOne.value
+var twovalue = crystal.crystaltwo.value
+var threevalue = crystal.crystalthree.value
+var fourvalue = crystal.crystalfour.value
 
 // jQuery click Functions
 
+function clickfunctions(){
+
  $("#one").click(function(){
-
- 	alert("alpha");
-
+ 	$("#currentscore").html(mycurrentscore+onevalue);
+ 	mycurrentscore = mycurrentscore + onevalue;
+ 
  });
 
 
  $("#two").click(function(){
- 	alert("beta");
+ 	$("#currentscore").html(mycurrentscore+twovalue);
+ 	mycurrentscore = mycurrentscore + twovalue;
 
  });
 
 
  $("#three").click(function(){
-	alert("gamma");
+	$("#currentscore").html(mycurrentscore+threevalue);
+ 	mycurrentscore = mycurrentscore + threevalue;
  });
 
  $("#four").click(function(){
-	alert("delta");
+	$("#currentscore").html(mycurrentscore+fourvalue);
+ 	mycurrentscore = mycurrentscore + fourvalue;
  });
 
+}
+
+clickfunctions()
+
+ // Win/Loose determination
+
+  	// var lossText = document.getElementById("losscount");
+   //  var winText  = document.getElementById("wincount");
+
+function winloose(){
+
+	
+
+	// if((mycurrentscore===goaltoreach)||(mycurrentscore>goaltoreach)){
+
+		if(mycurrentscore === goaltoreach){
+		
+		wincount++;
+		$("#wincount").html(wincount);
+	
+		console.log("you won");
+
+	}
+		else if (mycurrentscore > goaltoreach){
+			losscount++
+			$("#losscount").html(losscount);
+			alert("you lost");
+			console.log("you lost");
+		
+		}
+
+		// lossText.textContent = losscount;
+  //       winText.textContent = wincount;
+// console.log(wincount);
+// console.log(losscount);
+
+	}
+		// lossText.textContent = losscount;
+  //       winText.textContent = wincount;
+// console.log(wincount);
+// console.log(losscount);
+
+winloose();
+//         // lossText.textContent = losscount;
+//         // winText.textContent = wincount;
 
 
+// winloose(console.log( losscount));
+// winloose(console.log(mycurrentscore));
 
 
 
